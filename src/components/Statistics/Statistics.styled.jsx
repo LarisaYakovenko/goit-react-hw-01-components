d1b247d80dbace603ea205a52493bb423815553a
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2023-11-04 11:52:56 +0200

    update css file

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,15 +1,53 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// Section
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 0 auto;
+  margin-bottom: 30px;
+  max-width: 100%;
+  width: 400px;
+
+  box-shadow: -1px -1px 1px #fff, 1px 1px 1px #babecc;
+  border-radius: 8px;
+  box-shadow: -1px -1px 1px #fff, 1px 1px 1px #babecc;
+  border-radius: 8px;
+
+`;
+
+export const StatList = styled.div`
 
 
-// Title
+`;
 
-// StatList
-
-// Item
-
-// Label
+export const Item = styled.div`
 
 
-// Percentage
+`;
+export const Title = styled.div`
+  text-transform: uppercase;
+  padding: 20px;
+  align-items: centr;
+  color: #161616;
+  font-size: 36px;
+  font-weight: 600;
+  margin-bottom: 0px;
+  background-color: #eed7b8;
+
+
+`;
+export const Label = styled.div`
+
+
+`;
+export const Percentage = styled.div`
+
+
+`;
+
+
+
+
+
+
